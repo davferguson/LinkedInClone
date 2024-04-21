8243387e803169bb10d5957a6e72fc145c7da55b
Davin Ferguson <davferg43@gmail.com>
2024-04-21 01:24:47 -0400

    added a login page. can now register account through firebase auth. setup redux to save user state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Widgets from './Widgets';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import Login from './Login';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (curUser) => {
+      if(curUser) {
+        // user is logged in
+        dispatch(login({
+          email: curUser.email,
+          uid: curUser.uid,
+          displayName: curUser.displayName,
+          photoUrl: curUser.photoURL,
+        }))
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
+    {/* if no user, render login page */}
+    {!user ? <Login /> : (
       <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widgets */}
-        <Widgets />
-      </div>  
+      <Sidebar />
+      <Feed />
+      {/* Widgets */}
+      <Widgets />
+    </div>
+    )}
+        
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,10 @@
 // import firebase from "firebase/compat/app";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    // apiKey: "",
+    apiKey: "",
     authDomain: "linkedin-clone-50448.firebaseapp.com",
     projectId: "linkedin-clone-50448",
     storageBucket: "linkedin-clone-50448.appspot.com",
@@ -17,4 +18,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-export { db };
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export { db, auth };
